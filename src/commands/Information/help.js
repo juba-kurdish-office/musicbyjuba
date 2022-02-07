35198a16fa79a3a711cd1837620aace5c5448877
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-02-07 17:27:45 +0300

    Update help.js

--- src/commands/Information/help.js
+++ src/commands/Information/help.js
@@ -12,7 +12,7 @@ module.exports = {
       const embed = new MessageEmbed()
 
        .setTitle("**mix music - Help Menu**")
-       .setDescription(`Created By <@730549617044750346> if you need help join our [Community](https://discord.gg/gGzf9t3d6G)`)
+       .setDescription(`Created By <@730549617044750346> if you need help join our [Community](https://discord.gg/MXhMbtQ5)`)
     .addField("İnformation","`setprefix`, `invite`, `status`") 
     .addField("Music","`play`, `skip`, `stop`, `pause`, `resume`, `shuffle`, `seek`, `skipto`, `loop`, `remove`, `queue`, `filters`, `nowplaying`, `clearqueue`, `24/7`, `leave`") 
     .addField("Filter","`party`, `bass`, `radio`, `pop`, `trablebass`, `soft`, `custom`, `off`")
