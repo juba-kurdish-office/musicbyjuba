278a26a5ac69254eb1aa3137ee9fafa196214a56
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-02-07 17:33:05 +0300

    Update invite.js

--- src/commands/Information/invite.js
+++ src/commands/Information/invite.js
@@ -13,7 +13,7 @@ module.exports = {
       message.reply({
         embeds: [
           new MessageEmbed().setColor("RANDOM")
-          .setDescription(`Click [this Link](https://discord.com/api/oauth2/authorize?client_id=792047204361175091&permissions=294267518033&scope=bot%20applications.commands)`)
+          .setDescription(`Click [this Link](https://discord.com/api/oauth2/authorize?client_id=862989897954623500&permissions=1634503490528&scope=bot%20applications.commands)`)
         ]
       });
     }
