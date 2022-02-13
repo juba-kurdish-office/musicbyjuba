d66ff09d9e3acd874d1f7d93aa8d760c36a6ce06
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-02-13 16:32:05 +0300

    Update ready.js

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -5,7 +5,7 @@ module.exports = async (client) => {
     client.logger.log(`${client.user.username} online!`, "ready");
     client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`, "ready");
     //Game
-    let statuses = [`/help - ${guilds} servers`, `${prefix}help Created by juba`];
+    let statuses = ['/help - mix music', `${prefix}help Created by juba`];
     setInterval(function() {
   		let status = statuses[Math.floor(Math.random()*statuses.length)];
   		client.user.setActivity(status, {type: "PLAYING"});
