7a8ec17520b67666db5d39b89475f882bfbff0df
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-02-20 18:18:36 +0300

    Update invite.js

--- src/commands/Information/invite.js
+++ src/commands/Information/invite.js
@@ -1,20 +1,19 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const { MessageEmbed, CommandInteraction, Client, MessageButton, MessageActionRow } = require("discord.js")
 
 module.exports = {
     name: "invite",
-    category: "Information",
-    aliases: [ "addme" ],
-    description: "invite Remix Bot",
-    args: false,
-    usage: "",
-    permission: [],
-    owner: false,
-   execute: async (message, args, client, prefix) => {
-      message.reply({
-        embeds: [
-          new MessageEmbed().setColor("RANDOM")
-          .setDescription(`Click [this Link](https://discord.com/api/oauth2/authorize?client_id=862989897954623500&permissions=1634503490528&scope=bot%20applications.commands)`)
-        ]
-      });
+    description: "Get my invite link to join your servers!",
+
+    run: async (client, interaction) => {
+        await interaction.deferReply({
+            ephemeral: false
+        });
+
+         const mainPage = new MessageEmbed()
+             .setColor(client.embedColor)
+            .setDescription(`Support my bot join to your server
+ =========
+ [Link bot here](https://discord.com/api/oauth2/authorize?client_id=862989897954623500&permissions=1634503490528&scope=bot%20applications.commands)`)
+           await interaction.followUp({embeds: [mainPage]})
     }
-}
+		}
