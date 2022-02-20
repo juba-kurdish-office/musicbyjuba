6e600ed5f7d0aaed32b541262eac9bbf45d2ac96
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-02-20 18:14:48 +0300

    Update invite.js

--- src/slashCommands/Information/invite.js
+++ src/slashCommands/Information/invite.js
@@ -11,7 +11,8 @@ module.exports = {
 
          const mainPage = new MessageEmbed()
              .setColor(client.embedColor)
-            .setDescription(`click [this link](https://discord.com/api/oauth2/authorize?client_id=862989897954623500&permissions=1634503490528&scope=bot%20applications.commands)`)
+            .setDescription(`Support my bot join to your server
+ [Link bot here](https://discord.com/api/oauth2/authorize?client_id=862989897954623500&permissions=1634503490528&scope=bot%20applications.commands)`)
            await interaction.followUp({embeds: [mainPage]})
     }
 		}
