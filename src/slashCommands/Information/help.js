c9a4b1fc2e10a00bbfc96d4da0f0439b62f5f6db
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-02-22 13:31:14 +0300

    Add files via upload

---
+++ src/slashCommands/Information/help.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+    name: "help",
+    category: "Information",
+    aliases: [ "h" ],
+    description: "Return all commands, or one specific command",
+    args: false,
+    usage: "",
+    permission: [],
+    owner: false,
+ execute: async (message, args, client, prefix) => {
+      const embed = new MessageEmbed()
+
+       .setTitle("**mix music - Help Menu**")
+       .setDescription(`Created By <@730549617044750346> if you need help join our [Community](https://discord.gg/MXhMbtQ5)`)
+    .addField("İnformation","`setprefix`, `invite`, `status`") 
+    .addField("Music","`play`, `skip`, `stop`, `pause`, `resume`, `shuffle`, `seek`, `skipto`, `loop`, `remove`, `queue`, `filters`, `nowplaying`, `clearqueue`, `24/7`, `leave`") 
+    .addField("Filter","`party`, `bass`, `radio`, `pop`, `trablebass`, `soft`, `custom`, `off`")
+    .setColor("RANDOM")
+
+    message.channel.send({ embeds: [embed] }); 
+  }
+}
