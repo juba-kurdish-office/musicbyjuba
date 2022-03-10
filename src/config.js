eb0ec12be05ab3e76da7f2adb4e9a2ef2cbc1477
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2022-03-11 02:14:58 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    token: "ODYyOTg5ODk3OTU0NjIzNTAw.YOgX-Q.Xnp4tlKcfAl27pQh2Q-QhnL1SzI",
-    prefix: "m!",
-    ownerID: "730549617044750346",
+    token: "token-here",
+    ownerID: "your id",
     SpotifyID: "e9cedc5ab8414965ad62358f3ac47586",
     SpotifySecret: "666a22b234764df48ddb67997519e540",
     mongourl: "mongodb+srv://carloganjos12:carloganjos12@cluster0.vj8lc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     embedColor: "RANDOM",
-    logs: "945024066807541831",
+    logs: "channel id",
 
   nodes: {
      
